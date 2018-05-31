a0c68252295d1ce0c7ef384c6f2c86ce8dc2e893
James <jameschris.idn@gmail.com>
2018-05-31 22:01:20 +0700

    file Logic Challenge 1

---
+++ LogicChallenge1.js
@@ -0,0 +1,18 @@
+function bandingkanAngka(angka1, angka2) {
+    let result = "";
+    if( angka2 > angka1 ) {
+        result = true;
+    } else if ( angka1 > angka2 ) {
+        result = false;
+    } else if ( angka1 === angka2 ) {
+        result = -1;
+    }
+    return result;
+  }
+  
+  // TEST CASES
+  console.log(bandingkanAngka(5, 8)); // true
+  console.log(bandingkanAngka(5, 3)); // false
+  console.log(bandingkanAngka(4, 4)); // -1
+  console.log(bandingkanAngka(3, 3)); // -1
+  console.log(bandingkanAngka(17, 2)); // false
